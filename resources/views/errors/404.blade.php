000c1de0c0a1bd291fe7ec5a49abf4ab79c4f63f
khoirul55 <khoirulgunawan55@gmail.com>
2026-01-14 13:40:31 +0700

    404 error file

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center py-20">
+        <h1 class="text-4xl font-bold">404</h1>
+        <p>Halaman tidak ditemukan</p>
+    </div>
+@endsection
