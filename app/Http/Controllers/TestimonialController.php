21026bbd0da5fe6bd8c854f7d223eaec6ebec998
khoirul55 <khoirulgunawan55@gmail.com>
2026-01-19 14:49:20 +0700

    update TestimonialController

--- app/Http/Controllers/TestimonialController.php
+++ app/Http/Controllers/TestimonialController.php
@@ -16,7 +16,7 @@ class TestimonialController extends Controller
             'rating' => 5,
             'comment' => 'Alhamdulillah, pelayanan Mahira Tour sangat memuaskan. Hotel dekat dengan Masjidil Haram, pembimbing sangat ramah dan membantu. Perlengkapan lengkap, semuanya terorganisir dengan baik. Terima kasih Mahira Tour!',
             'date' => '2024-11-15',
-            'image' => asset('storage/gallery/jamaah3.jpeg'),
+            'image' => asset('storage/gallery/jamaah1.webp'),
         ],
         [
             'name' => 'Ibu Siti Fatimah',
@@ -25,7 +25,7 @@ class TestimonialController extends Controller
             'rating' => 5,
             'comment' => 'Pengalaman umrah yang luar biasa bersama Mahira Tour. Fasilitas VIP benar-benar premium, hotel sangat dekat dengan Masjidil Haram. Muthawwif sangat sabar dan membantu. Sangat recommended!',
             'date' => '2024-10-20',
-            'image' => asset('storage/gallery/jamaah3.jpeg'),
+            'image' => asset('storage/gallery/jamaah2.webp'),
         ],
         [
             'name' => 'Bapak Abdullah Hasan',
@@ -34,7 +34,7 @@ class TestimonialController extends Controller
             'rating' => 5,
             'comment' => 'Subhanallah, umrah di bulan Ramadhan bersama Mahira Tour sangat berkesan. Sahur dan berbuka bersama, kajian rutin, semuanya teratur. Air zam-zam juga banyak. Jazakumullah khairan!',
             'date' => '2024-09-08',
-            'image' => asset('storage/gallery/jamaah3.jpeg'),
+            'image' => asset('storage/gallery/jamaah3.webp'),
         ],
         [
             'name' => 'Ibu Aminah Zahra',
@@ -43,7 +43,7 @@ class TestimonialController extends Controller
             'rating' => 5,
             'comment' => 'Mahira Tour sangat profesional. Dari awal pendaftaran sampai pulang, semuanya lancar. Hotel Al Safwah sangat nyaman, makanan enak, dan tim sangat responsif. Insya Allah akan umrah lagi dengan Mahira Tour.',
             'date' => '2024-08-25',
-            'image' => asset('storage/gallery/jamaah3.jpeg'),
+            'image' => asset('storage/gallery/jamaah1.webp'),
         ],
         [
             'name' => 'Bapak Ridwan Kamil',
@@ -52,7 +52,7 @@ class TestimonialController extends Controller
             'rating' => 4,
             'comment' => 'Pelayanan bagus, harga kompetitif. Bus eksklusif sangat nyaman untuk transportasi. Perlengkapan lengkap semua sudah disediakan. Manasik sebelum berangkat juga sangat membantu. Recommended!',
             'date' => '2024-07-12',
-            'image' => asset('storage/gallery/jamaah3.jpeg'),
+            'image' => asset('storage/gallery/jamaah2.webp'),
         ],
         [
             'name' => 'Ibu Khadijah',
@@ -61,7 +61,7 @@ class TestimonialController extends Controller
             'rating' => 5,
             'comment' => 'Wisata halal ke Turki sangat menyenangkan! Tour guide ramah dan informatif. Semua tempat yang dikunjungi menarik. Makanan halal semua, hotel nyaman. Terima kasih Mahira Tour untuk pengalaman yang tak terlupakan!',
             'date' => '2024-06-30',
-            'image' => asset('storage/gallery/jamaah3.jpeg'),
+            'image' => asset('storage/gallery/jamaah3.webp'),
         ]
     ];
     
